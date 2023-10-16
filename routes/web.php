a18b32c5107815988ff2fe1cfc2e06516aa777ff
pddccvv <rudiyat404@gmail.com>
2023-10-16 08:50:30 +0700

    remove

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -29,4 +18,5 @@ Route::get('/dosen/login', function () {
 Route::get('/dosen/change-password', function () {
     return view('dosen.change-password');
 });
+
 
